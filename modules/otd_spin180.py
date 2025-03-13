07af4e523fdafd02fe7814e91f4116663d4e96a3
Kuroki <kohlrubenwinter@mrekk.ru>
2025-03-14 00:08:23 +0300

    Changed otd_spin180.py logic. TODO: write it

--- modules/otd_spin180.py
+++ modules/otd_spin180.py
@@ -1,11 +1,18 @@
 import subprocess
 
-class tabletRotator:
+def selectTablet(tabletList):
 
+    return tabletList[0]
+
+class tabletRotator:
 
     def __init__(self):
         """Проверка кол-ва планшетов"""
         self.tabletlist = subprocess.getoutput([]).split("\n")
+        if len(self.tabletlist) > 1:
+            self.selected = selectTablet(self.tabletlist)
+        else:
+            self.selected = self.tabletlist[0]
 
 
     def rotate(self):
